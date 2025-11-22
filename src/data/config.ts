d16d25747bd3f8bb12802d5e3d8d44dff3060552
treystr <67973734+treystr@users.noreply.github.com>
2025-11-22 18:00:59 -0500

    add calendar

--- src/data/config.ts
+++ src/data/config.ts
@@ -16,6 +16,7 @@ export const siteConfig = {
     { icon: 'tiktok', url: 'https://tiktok.com/@bitcoindistrict' },
     { icon: 'nostr', url: 'https://primal.net/p/nprofile1qqsgw7yl3m62uc0ffjl3ly5m8cqve63lprjdpkweejtemkl04r3vevsgzv27k' },
     { icon: 'linkedin', url: 'https://www.linkedin.com/company/bitcoindistrictinitiative' },
+    { icon: 'calendar', url: 'https://luma.com/bitcoindistrict?k=c' },
   ]
 };
 
